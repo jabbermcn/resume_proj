6de1234787707c9a9e733970845c104affb43032
mikhailouski_n <jabber_mcn@tut.by>
2023-03-14 14:26:19 +0300

    templates added

--- resume/urls.py
+++ resume/urls.py
@@ -4,4 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', HomeTemplateView.as_view(), name='home'),
+    path('about/', AboutTemplateView.as_view(), name='about'),
+    path('resume/', ResumeTemplateView.as_view(), name='resume'),
+    path('portfolio/', PortfolioTemplateView.as_view(), name='portfolio'),
+    path('contact/', ContactTemplateView.as_view(), name='contact'),
 ]
--- resume/views.py
+++ resume/views.py
@@ -5,12 +5,12 @@ class ContextMixin:
     context = {
         'site_title': '*****',
         'site_name': 'mikhailouski_n',
-        'facebook': 'https://facebook.com',
+        'facebook': 'https://www.facebook.com/jabber.mcn/',
         'twitter': 'https://twitter.com',
         'linkedin': 'https://www.linkedin.com/in/николай-михайловский-612744246/',
-        'instagram': 'https://instagram.com',
+        'instagram': 'https://www.instagram.com/mikhailouski_n/',
         'email': 'jabber_mcn@tut.by',
-        'Github': 'https://github.com/jabbermcn',
+        'github': 'https://github.com/jabbermcn',
         'phone': '+375 29 5692410',
     }
 
@@ -20,5 +20,41 @@ class HomeTemplateView(ContextMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeTemplateView, self).get_context_data()
+        context.update(self.context)
+        return context
+
+
+class AboutTemplateView(ContextMixin, TemplateView):
+    template_name = 'resume/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AboutTemplateView, self).get_context_data()
+        context.update(self.context)
+        return context
+
+
+class ResumeTemplateView(ContextMixin, TemplateView):
+    template_name = 'resume/resume.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ResumeTemplateView, self).get_context_data()
+        context.update(self.context)
+        return context
+
+
+class PortfolioTemplateView(ContextMixin, TemplateView):
+    template_name = 'resume/portfolio.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PortfolioTemplateView, self).get_context_data()
+        context.update(self.context)
+        return context
+
+
+class ContactTemplateView(ContextMixin, TemplateView):
+    template_name = 'resume/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactTemplateView, self).get_context_data()
         context.update(self.context)
         return context
